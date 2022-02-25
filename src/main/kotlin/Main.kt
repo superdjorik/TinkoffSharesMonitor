d26b050ffa68187c3e8bfee575c827792f41a6ae
Georgy Solonitsky <george@slntsk.ru>
2022-02-25 16:37:52 +0300

    Succesfully compiled as standalone jar application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,12 @@
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
-import io.ktor.client.HttpClient
-import io.ktor.http.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.network.sockets.*
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import patterns.secondDayGrows
-import stocksdata.GetStocks
-import stocksdata.Stock
 import java.io.File
-import java.net.ConnectException
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 
 suspend fun main() {
-
-    val path = "D:\\YandexDisk\\temp\\Tinkoff"
+//    val path = "D:\\YandexDisk\\temp\\Tinkoff"
     val dateTime = LocalDate.now()
 
     suspend fun <T> retryIO(
@@ -37,17 +22,20 @@ suspend fun main() {
                 return block()
             } catch (e: Exception) {
                 println(e.toString())
-                File(path, "${LocalDate.now()}_error.txt")
-                    .appendText("${LocalDateTime.now()} \nError: ${e.toString()}\n")
+                File("${LocalDate.now()}_error.txt")
+                    .appendText("${LocalDateTime.now()} \nError: ${e}\n")
             }
             delay(currentDelay)
             currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
         }
-        return block() // last attempt
+        return block()
     }
 
+
     val grows = retryIO { secondDayGrows() }
-    File(path, "${dateTime}_Растут.txt").writeText(grows.toString())
-    File(path, "log.txt").appendText("\n${dateTime} Finished successfull")
+
+
+    File("${dateTime}_Растут.txt").writeText(grows.toString())
+    File("log.txt").appendText("${LocalDateTime.now()} Finished successfull\n")
 
 }
